3eaa35498a2ecf8386fe90d6052ecb0b5be289ce
clo <clowenhg@mgail.com>
2024-05-29 19:05:32 -0400

    wip 2024-05-29

--- registry/cmd/main.go
+++ registry/cmd/main.go
@@ -8,15 +8,49 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 
+	"github.com/FosteredGames/Odyssey/registry/internal/config"
+	"github.com/FosteredGames/Odyssey/registry/internal/data"
 	"github.com/FosteredGames/Odyssey/registry/internal/identity"
+	"github.com/FosteredGames/Odyssey/registry/internal/registry"
 	"golang.org/x/oauth2"
 )
 
 func main() {
-	cfg, err := ConfigFromEnv()
+	// Ignoring stop func for now as we expect this to run until killed
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+
+	cfg, err := config.ConfigFromEnv()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	db, err := data.NewDB(context.Background(), cfg.DBConnection)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	reg := &registry.Registry{
+		DB:    db,
+		OAuth: &cfg.OAuth,
+	}
+
+	go func() {
+		reg.Run(ctx)
+		wg.Done()
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			wg.Wait()
+			return
+		}
+
 	}
 
 	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
@@ -35,7 +69,7 @@ func main() {
 		},
 	}
 
-	identity := identity.New(key, oauthConf)
+	identity := identity.New(key, oauthConf, db)
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	go identity.Run(ctx)
--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module github.com/FosteredGames/Odyssey/server
 
 go 1.22.2
+
+require (
+	github.com/urfave/cli/v3 v3.0.0-alpha9 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
