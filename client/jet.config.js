4b6adddf445a5f416a248f315c12076055ce6dc8
Christopher Lowenthal <clowenhg@gmail.com>
2024-11-27 06:57:39 -0500

    adds jest for testing

---
+++ client/jet.config.js
@@ -0,0 +1,13 @@
+export default {
+    "roots": [
+        "./src",
+        "./pb"
+    ],
+    "testMatch": [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+}
