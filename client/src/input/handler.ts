251de57328373dec7232d7c3636870d3f41dce20
Christopher Lowenthal <clowenhg@gmail.com>
2025-01-06 04:35:52 -0500

    fixes input handler to treat meta keys the same as normal keys

--- client/src/input/handler.ts
+++ client/src/input/handler.ts
@@ -28,13 +28,13 @@ export class KeyboardHandler {
 
         console.log("down", e.code, KeyboardHandler.keyCode(e))
 
-        this.state.keyDown(KeyboardHandler.keyCode(e))
+        this.state.keyDown(e.code)
     }
 
     protected keyUp(e: KeyboardEvent) {
         console.log("up", e.code, KeyboardHandler.keyCode(e))
 
-        this.state.keyUp(KeyboardHandler.keyCode(e))
+        this.state.keyUp(e.code)
     }
 
     /**
--- client/src/input/state.ts
+++ client/src/input/state.ts
@@ -25,23 +25,9 @@ export class KeyState {
 
     constructor() {
         this.keys = new Map<string, state>()
-
-        console.log("meta mod keys", metaModifiers.keys())
     }
 
     keyDown(code: string) {
-
-        // Meta keys aren't usable individually.
-        // Technical issue is that `keydown` will populate both the code and the
-        // specific meta key field.
-        // However, `keyup` will only have the code.
-        // So, with our hash `keyCode()`, this means that meta keys will never
-        // clear.
-        if (metaCodes.includes(code)) {
-            console.log("ignoring", code)
-            return
-        }
-
         let key = this.keys.get(code)
         if (!this.keys.has(code)) {
             key = new state(code)
